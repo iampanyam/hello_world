42d2bd95e0297b7d6fd04defe0297e18b1db4a5a
iampanyam <101621745+iampanyam@users.noreply.github.com>
2022-12-01 14:57:17 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,12 +1,12 @@
 class Example
 {
- static void main(String[] args)
- {
- // Defining a variable in lowercase
- int x=5;
- // Defining a variable in uppercase
- int X=6;
- // Defining a variable with the underscore in it's name
- def _Name="Joe";
- }
+    static void main(String[] args)
+   {
+       // Defining a variable in lowercase
+         int x=5;
+         // Defining a variable in uppercase
+         int X=6;
+       // Defining a variable with the underscore in it's name
+         def _Name="Joe";
+    }
 }
